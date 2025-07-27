0b9aca33125c2c2ddbcd706c341714497961e2a8
shimmymary98 <shimmymary060@gmail.com>
2025-07-27 16:13:06 +0300

    new changes

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -10,34 +10,12 @@ router.get('/customers',(req,res)=>{
 })
 
 router.get('/agents',(req,res)=>{
-    res.render('dashboardagents');
+    res.render('dashboardagent');
 })
 
 router.get('/manager',(req,res)=>{
     res.render('dashboardmanager');
 })
-
-
-
-
-
-
-
-// router.post('/addchicks',(req,res)=>{
-//     const chickName = req.body.chickName;
-//     const chickAge = req.body.chickAge;
-//     const chickBreed = req.body.chickBreed;
-
-//     // Here you would typically save the chick data to a database
-//     // For this example, we'll just send a response back
-
-//     res.send(`Chick added: Name - ${chickName}, Age - ${chickAge}, Breed - ${chickBreed}`);
-// });
-
-// router.post('/addchicks',(req,res)=>{
-//     console.log(req.body);
-//     // res.send('Chick added successfully');
-// })
 
 module.exports=router
 
